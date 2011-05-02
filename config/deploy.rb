814a07e16f3c30f54847ca2d25d7eb63423529d1
Nicolas Viennot <nicolas@viennot.biz>
2011-05-02 05:04:13 -0400

    Build and Install

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,28 @@ set :keep_releases, 5
 
 role :racepro, 'racepro'
 
+libs = [
+  {
+    :name => 'linux-2.6-scribe',
+    :dependencies => ['make', 'gcc'],
+    :build_cmd => ['ln -fs ../config/kernel_config .config', 'make -j4'],
+    :install_cmd => ['make install', 'make modules_install',
+                     'mkinitramfs -o /boot/initrd.img-2.6.35-scribe+ 2.6.35-scribe+']
+  }, {
+    :name => 'libscribe',
+    :dependencies => 'cmake',
+    :work_dir => 'build',
+    :build_cmd => ['cmake ..', 'make'],
+    :install_cmd => 'make install'
+  }, {
+    :name => 'py-scribe',
+    :dependencies => ['python-dev', 'cython'],
+    :build_cmd => ['python setup.py build'],
+    :install_cmd => ['python setup.py install']
+  }
+]
+
+########## Helpers ##########
 def copydir(src, dst)
   filename = '/tmp/cap.copy.tar.gz'
   remote_filename = filename
@@ -26,6 +48,11 @@ ensure
   File.delete filename rescue nil
 end
 
+def install(packages)
+  sudo "apt-get -q -y install #{packages.to_a.join(' ')}"
+end
+
+########## Tasks ##########
 namespace :bootstrap do
   task :users do
     users = Dir.entries('users').select { |d| not d.match('^\.') }
@@ -34,8 +61,9 @@ namespace :bootstrap do
     users.each { |user| sudo "chown -R #{user}: /home/#{user}" }
   end
 
-  task :packages do
-    sudo "apt-get -q -y install git-core vim-nox"
+  task :dependencies do
+    install(['git-core', 'vim-nox'])
+    libs.each { |lib| install(lib[:dependencies]) if lib[:dependencies] }
   end
 
   task :known_hosts do
@@ -45,7 +73,7 @@ namespace :bootstrap do
   desc 'bootstrap the server'
   task :default do
     users
-    packages
+    dependencies
     known_hosts
     deploy.setup
   end
@@ -56,4 +84,12 @@ namespace :deploy do
     sudo "chown #{user} -R #{deploy_to}"
   end
   after "deploy:setup", "deploy:fix_permissions"
+
+  task :finalize_update do
+    libs.each do |lib|
+      work_dir = File.join(latest_release, lib[:name], lib[:work_dir].to_s)
+      cmds = lib[:build_cmd].to_a + lib[:install_cmd].to_a.map { |c| "#{sudo} #{c}" }
+      run "cd #{work_dir} && #{cmds.join(' && ')}"
+    end
+  end
 end
